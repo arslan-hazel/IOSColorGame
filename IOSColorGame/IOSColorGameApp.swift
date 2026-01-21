b6fcb89b0c3a9a59ac3bfe16a50572ab3534fee3
Arslan Ali <arslan.ali@imaginationai.net>
2026-01-21 14:35:35 +0500

    Initial Commit

---
+++ IOSColorGame/IOSColorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSColorGameApp.swift
+//  IOSColorGame
+//
+//  Created by Arslan on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOSColorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
